1f472a14690058a04c0a7b9c866bd0a2f1a5adfb
Rhys Aislabie <rdaislabie@gmail.com>
2016-02-14 16:54:22 +0000

    Challenge1 done. Initial commit.

---
+++ RedditDailyProgrammer/Challenge1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditDailyProgrammer
+{
+    class Challenge1
+    {
+        public Challenge1()
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+            Console.WriteLine("What is your username?");
+            var userName = Console.ReadLine();
+            Console.WriteLine("Ok so your name is" + name + ", you are" + age + "years old and your username is" + userName + ".");
+            Console.ReadKey();
+        }
+    }
+}
